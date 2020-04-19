10530109c5d9478b7c1f4a7bd04f1a6512fbeaa5
Mathias de Mestral <mathiasdemestral@gmail.com>
2020-04-19 08:26:49 -0400

    Agrega Speciality y relacion con Vet. Closes #27

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Speciality extends BaseEntity {
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    private String description;
+}
