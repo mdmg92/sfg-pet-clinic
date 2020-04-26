5f010c2771c4cd78b96796466f4a7d2c40c5d385
Mathias de Mestral <mathiasdemestral@gmail.com>
2020-04-26 12:03:32 -0400

    Agrega Visit a Pets en startup. Closes #48

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VisitService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VisitService.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Visit;
+import org.springframework.stereotype.Service;
 
 public interface VisitService extends CrudService<Visit, Long> {
 }
